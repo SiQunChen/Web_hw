5827ea9639c7768a9296698b8339b4b900861dd6
SiQunChen <92172174+SiQunChen@users.noreply.github.com>
2022-11-03 00:34:46 +0800

    Update script.js

--- LAB5/script.js
+++ LAB5/script.js
@@ -1,1 +1,24 @@
-//write your script here
+function reset(){
+    chgWidth("300");
+    chgColor("gray");
+    chgBorder("1");
+}
+function chgColor(color){
+    var a = document.getElementsByTagName('td');
+    for (var i=0;i<a.length;i++){
+        a[i].style.backgroundColor = color;
+    }
+}
+function chgBorder(w){
+    var a = document.getElementsByTagName('td');
+    for (var i=0;i<a.length;i++){
+        a[i].style.borderWidth = w+'px';
+    }
+    a = document.getElementsByTagName('th');
+    for (var i=0;i<a.length;i++){
+        a[i].style.borderWidth = w+'px';
+    }
+}
+function chgWidth(h){
+    document.getElementById('table').width = h+"px";
+}
